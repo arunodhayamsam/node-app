6cd758c16f88e3d301ce2f963e5dfa04dac037d1
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2022-08-08 12:55:00 +0530

    Added the index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Hello World from dev!")
+    res.send("Hello World from dev and I am from the Github Actions!")
 });
 
 app.listen(80,() => {
