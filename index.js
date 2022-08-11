cdd227f1de58cecfead5c9abace6898482cbe21f
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2022-08-11 19:43:59 +0530

    Final test for merge to MAIN

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Hello World from dev and I am from the Github Actions!")
+    res.send("Hello World from dev and I am from the Kubectl github action!")
 });
 
 app.listen(80,() => {
