a67f63a40fa9105b5825cb95842c2a62a8673a46
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2022-09-30 00:34:35 +0530

    testconfig

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Hello World from dev and I am from the Kubectl github action!")
+    res.send("Ecs-test")
 });
 
 app.listen(80,() => {
